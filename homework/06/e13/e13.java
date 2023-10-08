01c56a1058c1237ea6a394208564c1b4a6217e28
Lauri Elsilä <lauri.elsila@tuni.fi>
2023-10-09 00:59:49 +0300

    Muokattiin tehtävää e13

--- homework/06/e13/e13.java
+++ homework/06/e13/e13.java
@@ -2,7 +2,26 @@ import java.io.Console;
 
 
 class e13 {
-    static String [] hedelm = {"@", "!", "#", "&"};
+    //koodille universaaleja muuttujia,
+    //eli itse eri hedelmät,
+    //peliruudun sisältötaulukko
+    //ja readLineen c-muuttuja
+
+    //värit (ANSI)
+    static String kel = "\u001B[33m";
+    static String pun = "\u001B[31m";
+    static String vih = "\u001B[32m";
+    static String sin = "\u001B[34m";
+
+    //resettiväri
+    static String res = "\u001B[0m";
+
+    static String [] hedelm = {
+        kel + "@" + res, 
+        pun + "!" + res, 
+        vih + "#" + res, 
+        sin + "&" + res
+        };
     //oletusarvo huutomerkkejä
     static String [] ruutu = {"!", "!", "!", "!", "!"};
     static Console c = System.console();
@@ -12,7 +31,15 @@ class e13 {
         int summa = 0; //pelaajan yhteensä käyttämät rahat
         int putki = 0; //montako merkkiä putkeen
         int korkein = 0; //korkein putki
-        System.out.println("Tervetuloa HedelmäPeliin!");
+        int delay = 10; //millisekunnit uuden printin välillä sekoituksessa
+        int kum = 0; //kumitus, extra linjoja joita poistaa kumita() funktiota varten
+        System.out.println(
+            vih + "Tervetuloa HedelmäPeliin!" +
+            pun + "\nPelaa vastuullisesti." +
+            "\nHedelmäPeli oy tai sen sisaryhtiöt" +
+            "\neivät vastaa menetetystä pääomasta.\n\n" +
+            res
+            );
 
         //niin kauan kunnes käyttäjä lopettaa
         while(true)
@@ -23,29 +50,55 @@ class e13 {
                 );
             String vastaus = c.readLine();
 
+            //helvetin pitkä if-lause joka pyörittää itse peliä,
+            //jos pelaaja valitsee jatkavansa siis
             if(vastaus.equals("y") && raha > 0)
             {
-                summa += 1;
-                raha -= 1;
-
+                //muuttujien asettelu
+                summa += 1; //euro käytetty, laitetaan muistiin
+                raha -= 1; //euro per peli
                 //oletusarvo yksi (eli yksi peräkkäinen merkki)
                 putki = 1;
                 korkein = 1;
+                //delay minimiin ennen looppia
+                delay = 10;
 
+                //tyhjentää rivit ja nollaa kum-muuttujan
+                kumita(6+kum);
+                kum = 0;
                 //satunnaiset hedelmät
                 shedelmiä();
-
                 //ruudun tulostus
                 printR();
+                
+                while(delay < 100)
+                {
+                    //tyhjennä rivi, random hedelmät,
+                    //printtaa ne, odota hetki,
+                    //hidasta tahtia,
+                    //uudestaan
+                    kumita(1);
+                    shedelmiä();
+                    printR();
+                    odota(delay);
 
+                    delay += 5;
+                }
+
+                //0,7 sekunnin väli, ettei hyppää naamalle
+                odota(700);
+
+                //spacing väliin
+                System.out.println();
+                
                 //tarkistetaan ruudut, aloitetaan yhdestä
                 for(int i = 1; i < ruutu.length; i++)
                 {
                     if(!ruutu[i].equals(ruutu[i-1]))
                     {
-                        //jos jäsen i ei ole sama kuin aiempi jäsen,
+                        //jos jäsen i ei ole sama kuin yksi aiempi jäsen,
                         //kaikki jäsenet eivät ole samoja;
-                        //mutta jos koko matkan jäsenet ovat samoja,
+                        //mutta jos koko matkan peräkkäiset jäsenet ovat samoja,
                         //kaikki taulukon jäsenet ovat samoja
                         if(putki > korkein)
                         {
@@ -67,8 +120,9 @@ class e13 {
                 //saa tietenkin voittoja
                 if(korkein == ruutu.length) //kaikki samaa, jackpot
                 {
-                    System.out.println("Voitit jackpotin!");
-                    raha += 10;
+                    System.out.println(vih + "Voitit jackpotin!" + res);
+                    raha += 25; //tienataan 25e
+                    kum += 1; //extralinja, joka pitää poistaa
                 }
                 else if(korkein > 2) // 3-4 putki
                 {
@@ -77,8 +131,9 @@ class e13 {
                         "hedelmää! Voitit " + korkein +
                         " euroa.");
                     raha += korkein;
+                    kum += 1;
                 }
-            }
+            } //NYT VASTA käsitellään virheinputit tai kieltymys (n)
             else if(vastaus.equals("n"))
             {
                 System.out.println("Peli ohi.");
@@ -95,7 +150,12 @@ class e13 {
             }
             else
             {
-                System.out.println("Virheellinen syöte!");
+                System.out.println(
+                    pun +
+                    "Virheellinen syöte!" +
+                    res
+                    );
+                kum += 5;
             }
         }
     }
@@ -122,6 +182,31 @@ class e13 {
         {
             System.out.print("[" + ruutu[i] + "]");
         }
-        System.out.println();
+    }
+
+    //odottaa annetun määrän ms
+    public static void odota(int ms)
+    {
+        try{
+            Thread.sleep(ms);
+            } catch(Exception e){
+                //ei mitään tarvetta tälle
+            }
+    }
+
+    public static void kumita(int rivei)
+    {
+        //mennään rivin alkuun ja kumitetaan koko rivi
+        System.out.print("\r");
+        System.out.print("\033[2K");
+
+        //jos rivei enemmän kuin yksi, menemme for looppiin
+        for(int i = 1; i < rivei; i++)
+        {
+            //liikkuu yhden ylöspäin
+            System.out.print(String.format("\033[%dA", 1));
+            System.out.print("\r");
+            System.out.print("\033[2K");
+        }
     }
 }
